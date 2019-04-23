b6bcd2652b8bf62b88c5d3c882ca937e9978b1e9
Eduardo Enriquez <eduardo.enriquez@wizeline.com>
2019-04-23 17:31:38 -0500

    WIP

--- tiny-url/pkg/url/controller.go
+++ tiny-url/pkg/url/controller.go
@@ -1,9 +1,24 @@
 package pkg
 
-const serviceURL = "http://localhost:8080/"
+const (
+	serviceURL = "http://localhost:8080/"	
+	fakeUrl = 125
+	alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
+)
+
+func encode(url string) []int{
+	remainder := 125 // this should bne url param
+	var remainderSlice []int
+	for remainder > 0 {
+		remainderSlice = append(remainderSlice, (remainder % 62))
+		remainder = remainder / 62
+	}
+	return remainderSlice
+}
 // URLShortener should return given url shortened
 func URLShortener(url string) string {
 	// encode
+	encode(url)
 	return serviceURL + "123abc"
 }
